a7fa1858dc3bf24b39c00320e9c23f0bc940aa41
xianfengting <1142631421@qq.com>
2019-07-12 21:24:11 +0800

    Fix several bugs of EventBusObservableProperty

--- src/main/java/com/src_resources/kerlib/eventbus/EventBusObservableProperty.kt
+++ src/main/java/com/src_resources/kerlib/eventbus/EventBusObservableProperty.kt
@@ -3,34 +3,32 @@ package com.src_resources.kerlib.eventbus
 import kotlin.properties.ObservableProperty
 import kotlin.reflect.KProperty
 
-abstract class EventBusObservableProperty<T>(initialValue: T,
+class EventBusObservableProperty<T>(initialValue: T,
                                              val targetEventBus: EventBus,
-                                             val beforeChangeCallable: (EventBusObservableProperty<*>, T, T) -> Boolean
+                                             val beforeChangeCallable: (KProperty<*>, T, T) -> Boolean
                                                     = { _, _, _ -> true },
-                                             val afterChangeCallable: (EventBusObservableProperty<*>, T, T) -> Unit
+                                             val afterChangeCallable: (KProperty<*>, T, T) -> Unit
                                                     = { _, _, _ -> },
                                              val async: Boolean = false)
             : ObservableProperty<T>(initialValue) {
-    class BeforeChangeEvent<T>(val targetProperty: EventBusObservableProperty<*>,
+    class BeforeChangeEvent<T>(val targetProperty: KProperty<*>,
                                val oldValue: T,
                                val newValue: T,
                                override var cancelled: Boolean = false) : Event(), Event.Cancellable
-    class AfterChangeEvent<T>(val targetProperty: EventBusObservableProperty<*>,
+    class AfterChangeEvent<T>(val targetProperty: KProperty<*>,
                               val oldValue: T,
                               val newValue: T) : Event()
 
     override fun beforeChange(property: KProperty<*>, oldValue: T, newValue: T): Boolean {
-        val propertyObj = property as EventBusObservableProperty<*>
-        val cancelled = beforeChangeCallable(propertyObj, oldValue, newValue)
-        val event = BeforeChangeEvent(propertyObj, oldValue, newValue, cancelled)
+        val cancelled = beforeChangeCallable(property, oldValue, newValue)
+        val event = BeforeChangeEvent(property, oldValue, newValue, cancelled)
         targetEventBus.postEvent(event, async)
-        return event.cancelled
+        return !event.cancelled
     }
 
     override fun afterChange(property: KProperty<*>, oldValue: T, newValue: T) {
-        val propertyObj = property as EventBusObservableProperty<*>
-        afterChangeCallable(propertyObj, oldValue, newValue)
-        val event = AfterChangeEvent(propertyObj, oldValue, newValue)
+        afterChangeCallable(property, oldValue, newValue)
+        val event = AfterChangeEvent(property, oldValue, newValue)
         targetEventBus.postEvent(event, async)
     }
 }
